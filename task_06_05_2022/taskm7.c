9332963b2e6faf67ab9b3cd60a5f9ce7d0416a9f
sravanthidane <104495700+sravanthidane@users.noreply.github.com>
2022-05-10 12:59:41 +0530

    first commit

---
+++ task_06_05_2022/taskm7.c
@@ -0,0 +1,41 @@
+// c program to check if a string ends with a substring//
+#include <stdio.h>
+#include <string.h>
+void check_substring(char str[], char substr[])
+{
+    int c, lenofsubstr, lenofstr;
+    if (lenofsubstr < lenofstr)
+    {
+        for (int i = 0; i < lenofsubstr; i++)
+        {
+            if (str[i + lenofstr - lenofsubstr] != substr[i])
+            {
+
+                c == 0;
+            }
+        }
+    }
+
+    if (c == 1)
+    {
+        printf("String ends with substring.\n");
+    }
+
+    else
+    {
+        printf("String does not  end with substring.\n");
+    }
+}
+
+int main()
+{
+    char str[30] = "capital of india";
+    char substr[30] = "india";
+    int c, i;
+    int lenofstr = strlen(str);
+    int lenofsubstr = strlen(substr);
+
+    // logic to check and traversing string//
+    check_substring(str, substr);
+    return 0;
+}
