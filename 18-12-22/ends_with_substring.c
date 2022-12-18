672765cf423efb04dbed0b8631758b06d59811c8
sravanthidane <104495700+sravanthidane@users.noreply.github.com>
2022-12-18 18:30:48 +0530

    practised c programs

---
+++ 18-12-22/ends_with_substring.c
@@ -0,0 +1,26 @@
+//c program to check whether a string ends with given substring//
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+bool char_ends(char str[], char substr[])
+{
+    int l1 = strlen(str);
+    int l2 = strlen(substr);
+    if (l2 > l1)
+        return false;
+    for (int i = 0; i < l2; i++)
+        if (str[l1 - i] != substr[l2 - i])
+            return false;
+}
+int main()
+{
+
+    char str[] = "iam doing well";
+    char substr[] = "ll";
+
+    if (char_ends(str, substr))
+        printf("string doesnot ends with substring");
+    else
+        printf("string ends with substring");
+    return 0;
+}
