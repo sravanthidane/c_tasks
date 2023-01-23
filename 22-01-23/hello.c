e37a34b6740e644de9f080d4119216fdb8079799
sravanthidane <104495700+sravanthidane@users.noreply.github.com>
2023-01-23 12:49:07 +0530

    weekly practised programs

---
+++ 22-01-23/hello.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include"header.h"
+void hello(){
+    printf("welcome to c\n");
+}
---
+++ 22-01-23/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"header.h"
+int main(){
+    printf("in main:\n");
+    add(4,6);
+    hello();
+    return 0;
+}
