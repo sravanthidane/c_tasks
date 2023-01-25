521913cb5d305188192bc619ef5e61517eb3c899
sravanthidane <104495700+sravanthidane@users.noreply.github.com>
2023-01-25 12:36:52 +0530

    weekly practised programs

---
+++ 15-01-23/pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s[100];
+    int i,j;
+    printf("enter string\n");
+    scanf("%[^\n]s",s);
+    for(i=0;s[i]!='\0';i++){
+        for(j=0;j<=i;j++){
+            printf("%c",s[j]);
+        }
+        printf("\n");
+   
+}
